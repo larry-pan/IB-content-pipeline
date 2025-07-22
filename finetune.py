006f6ba24305b47416cfaefed74c0e36de9b2257
larry-pan <l55pan@uwaterloo.ca>
2025-07-21 20:50:44 -0400

    first finetuning

---
+++ finetune.py
@@ -0,0 +1,32 @@
+import cohere
+from dotenv import dotenv_values
+from cohere.finetuning import Settings, FinetunedModel, BaseModel
+
+MODEL_NAME = "test-finetune-model"
+
+
+config = dotenv_values(".env")
+co = cohere.ClientV2(config.get("COHERE_KEY"))
+
+chat_dataset = co.datasets.create(
+    name="chat-dataset-with-eval",
+    data=open("training.jsonl", "rb"),
+    eval_data=open("eval.jsonl", "rb"),
+    type="chat-finetune-input",
+)
+result = co.wait(chat_dataset)
+print(result)
+
+print("------------")
+create_response = co.finetuning.create_finetuned_model(
+    request=FinetunedModel(
+        name="test-finetune-model",
+        settings=Settings(
+            base_model=BaseModel(
+                base_type="BASE_TYPE_CHAT",
+            ),
+            dataset_id=chat_dataset.id,
+        ),
+    ),
+)
+print(create_response)
